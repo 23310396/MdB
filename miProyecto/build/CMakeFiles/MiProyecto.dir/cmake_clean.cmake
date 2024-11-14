fe54f01469c8d8b4efde920ea4be557022c62632
23310396 <a23310396@live.ceti.mx>
2024-11-14 01:56:16 +0000

    Paso 1

---
+++ miProyecto/build/CMakeFiles/MiProyecto.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MiProyecto.dir/src/main.cpp.o"
+  "MiProyecto"
+  "MiProyecto.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MiProyecto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ miProyecto/src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "¡Hola, Codespaces!" << std::endl;
+    return 0;
+}
