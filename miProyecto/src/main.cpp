32d3b77b7aaf0c6e14f17ae1e33e50e87f24cb5a
23310396 <a23310396@live.ceti.mx>
2024-11-14 02:03:59 +0000

    Paso 2

--- miProyecto/src/main.cpp
+++ miProyecto/src/main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
-
+#include <string>
+#include "exprtk.hpp" 
+ 
+ using namespace std;
 int main() {
-    std::cout << "¡Hola, Codespaces!" << std::endl;
+    string expression_str;
+    cout << "Introduce una función de x (ej. sin(x), exp(x), log(x), x^2 + 3*x): ";
+    getline(cin, expression_str);
+
+    exprtk::symbol_table<double> symbol_table;
+    double x;
+    symbol_table.add_variable("x", x);
+    symbol_table.add_constants();
+
+    exprtk::expression<double> expression;
+    expression.register_symbol_table(symbol_table);
+
+    exprtk::parser<double> parser;
+    if (!parser.compile(expression_str, expression)) {
+        cerr << "Error en la función introducida: " << expression_str << endl;
+        return 1;
+    }
+
+    cout << "Tabulando la función para valores de x entre -3 y 3:" << endl;
+    for (x = -3; x <= 3; x += 0.5) {
+        cout << "x = " << x << ", f(x) = " << expression.value() << endl;
+    }
+
     return 0;
 }
